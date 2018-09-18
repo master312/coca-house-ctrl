accbc78cdb7ee00d4b1dac9ecf3383d19a7baf7f
master312 <wowhellmail@gmail.com>
2018-09-18 19:14:52 +0200

    Added pm2 deploy system.

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps : [{
+    name      : 'COCA-HOUSE-CTRL',
+    script    : 'app.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    },
+    env_staging : {
+      // ENVIRONMENT VARIABLES FOR STAGING
+    }
+  }],
+
+  deploy : {
+    staging : {
+      user : 'node',
+      host : '192.168.1.199',
+      ref  : 'origin/testing',
+      repo : 'git@github.com:master312/coca-house-ctrl.git',
+      path : '/home/deploy/coca-house-ctrl',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    
+    production : {
+      user : 'node',
+      host : '192.168.1.199',
+      ref  : 'origin/origin',
+      repo : 'git@github.com:master312/coca-house-ctrl.git',
+      path : '/home/deploy/coca-house-ctrl',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
